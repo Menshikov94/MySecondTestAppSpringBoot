5702dfa21da745539812ab4e64fec694434388c3
Maksim <menshikovma@yandex.ru>
2023-10-07 09:31:22 +0500

    Lr3 commit 4

--- src/main/java/ru/menshikov/MySecondTestAppSpringBoot/service/RequestValidationService.java
+++ src/main/java/ru/menshikov/MySecondTestAppSpringBoot/service/RequestValidationService.java
@@ -1,14 +1,17 @@
 package ru.menshikov.MySecondTestAppSpringBoot.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
 import ru.menshikov.MySecondTestAppSpringBoot.exception.ValidationFailedException;
 
+@Slf4j
 @Service
 public class RequestValidationService implements ValidationService {
 @Override
     public void isValid(BindingResult bindingResult) throws ValidationFailedException {
     if (bindingResult.hasErrors()) {
+        log.warn("Сообщение из bindingResult: " + bindingResult.getFieldError().getDefaultMessage());
         throw new
                 ValidationFailedException(bindingResult.getFieldError().toString());
     }
